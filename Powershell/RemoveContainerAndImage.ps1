a1bb31f834b1de0978eb488290a5357120a99c0e
jinxuunity <jinxu@unity3d.com>
2017-06-11 17:11:41 +0200

    Delete Container and its image.

---
+++ Powershell/RemoveContainerAndImage.ps1
@@ -0,0 +1,24 @@
+﻿<#
+    .DESCRIPTION
+        Remove the container and its image.
+    .DEPENDENCY
+        Powershell Docker module.
+        https://github.com/Microsoft/Docker-PowerShell
+#>
+
+Param (
+    [string]$name
+)
+
+Write-Host $name
+
+Stop-Container $name
+$image = (Get-Container -ContainerIdOrName $name).Image
+Remove-Container $name
+
+# Looking for other container which also use this image
+$other_container = (Get-Container | Where-Object {$_.Image -eq $image}).Names
+Stop-Container $other_containers
+Remove-Container $other_container
+
+Remove-ContainerImage $image
